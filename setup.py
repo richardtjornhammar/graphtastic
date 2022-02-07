609c16353b5468b135bdc91cfb044a5fdfb494f9
Richard Tjörnhammar <richard.tjornhammar@gmail.com>
2022-02-07 10:01:05 +0100

    gt 0.10.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "graphtastic",
-    version = "0.10.0",
+    version = "0.10.1",
     author = "Richard Tjörnhammar",
     author_email = "richard.tjornhammar@gmail.com",
     description = "Graphtastic, a Statistical Graph Learning library for Humans",
